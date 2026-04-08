585151494a8089fe06686145d751938fbb8a7dfc
nbploc08 <trongnghia1112223333@gmail.com>
2026-04-08 19:10:35 +0700

    feat: integrate job queue into posts module for enhanced processing

--- apps/content-service/src/modules/posts/posts.module.ts
+++ apps/content-service/src/modules/posts/posts.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
+import { QueueModule } from 'src/modules/queue/queue.module';
 
 @Module({
+  imports: [QueueModule],
   controllers: [PostsController],
   providers: [PostsService],
   exports: [PostsService],
